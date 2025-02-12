3b670cbd93b1cd9f5bb6a5034dbc73baae8f4a5c
ViljaVellamo <tuuli.v.hongisto@gmail.com>
2025-02-12 16:02:44 +0200

    Code simplified.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import csv
 import random
 import string
-from random import choice
 
 times_correct = 0
 times_looped = 0
 generated_numbers_poets = []
 generated_numbers_chatgpt = []
 accuracy = 0
-language = 0
 
 def fetch_poem (file_number, row_number):
 
     with open ('Table ' + str(file_number) + '.csv', newline='', encoding='utf-8-sig') as csvfile:
-
         reader = csv.reader(csvfile)
         poem_from_table = ""
         rows = list(reader)
@@ -24,56 +21,51 @@ def fetch_poem (file_number, row_number):
     return poem_from_table
 
 while True:
-    language_from_user = input("Welcome to the game of 'Is it human or ChatGPT'.\n"
+    language = input("Welcome to the game of 'Is it human or ChatGPT'.\n"
                                "You will be shown a poem that can be either written by a human or by ChatGPT.\n"
                                "To choose the language of the poems, press 'en' for English, 'fi' for Finnish, "
                                "or 'both' for both.\n")
 
-    if language_from_user == "fi":
-        language += 1
-    if language_from_user == "both":
-        language += 2
-    if language_from_user != "fi" and language_from_user != "en" and language_from_user != "both":
+    if language != "fi" and language != "en" and language != "both":
         print("Please choose the language by typing 'en' for English, 'fi' for Finnish, or 'both' for both.\n")
         continue
 
     while True:
 
-        number_int = input("Press x to show a poem. \n"
-                           "If you wish to end the program, press y. \n")
+        user_input = input("Press x to show a poem. \n"
+                           "If you wish to end the program, press q. \n")
 
-        if number_int == "y":
+        if user_input == "q":
             print("Thank you for playing! You have an accuracy rate of " + str(accuracy) + "%")
             quit()
 
-        if str(number_int) != "x" and str(number_int) != "y":
+        if str(user_input) != "x" and str(user_input) != "y":
             print("Please press x to continue or y to end the program.\n")
 
-        if number_int == "x":
+        if user_input == "x":
+            file_number = random.randint(1, 2)
             row_number = 0
-            random_file_number = random.randint(1, 2)
 
-            random_int = 0
             i = 0
+            while i < 1000000:
+                if language == "en":
+                    row_number = random.randint(0, 55)
+                if language == "fi":
+                    row_number = random.randint(56, 99)
+                if language == "both":
+                    row_number = random.randint(0, 99)
 
-            if random_file_number == 1:
-                if language == 0:
-                    random_int = choice([i for i in range(0, 55) if i not in [generated_numbers_poets]])
-                if language == 1:
-                    random_int = choice([i for i in range(56, 99) if i not in [generated_numbers_poets]])
-                if language == 2:
-                    random_int = choice([i for i in range(0, 99) if i not in [generated_numbers_poets]])
-                generated_numbers_poets.append(random_int)
-            else:
-                if language == 0:
-                    random_int = choice([i for i in range(0, 55) if i not in [generated_numbers_chatgpt]])
-                if language == 1:
-                    random_int = choice([i for i in range(56, 99) if i not in [generated_numbers_chatgpt]])
-                if language == 2:
-                    random_int = choice([i for i in range(0, 99) if i not in [generated_numbers_chatgpt]])
-                generated_numbers_chatgpt.append(random_int)
+                if file_number == 1:
+                    if row_number not in generated_numbers_poets:
+                        generated_numbers_poets.append(row_number)
+                        break
+                else:
+                    if row_number not in generated_numbers_chatgpt:
+                        generated_numbers_chatgpt.append(row_number)
+                        break
+                i += 1
 
-            poem = fetch_poem(random_file_number, random_int)
+            poem = fetch_poem(file_number, row_number)
             print("\n" + poem)
             print("\n")
 
@@ -87,21 +79,21 @@ while True:
                     with open ("Table 1 only  authors.csv", newline='', encoding='utf-8-sig') as csvfile:
                         reader = csv.reader(csvfile)
                         rows = list(reader)
-                        author_name = str(rows[random_int]).translate(str.maketrans('', '', string.punctuation))
+                        author_name = str(rows[row_number]).translate(str.maketrans('', '', string.punctuation))
 
                     print("")
 
-                    if human_or_chatGPT == str(random_file_number):
+                    if human_or_chatGPT == str(file_number):
                         times_correct += 1
                         if human_or_chatGPT == "1":
-                            print("You are right. This poem was written by " + str(author_name) + ".")
+                            print("You are right. This poem was written by ", author_name + ".")
                         if human_or_chatGPT == "2":
                             print("You are right. This poem was written by ChatGPT.")
-                    if human_or_chatGPT != str(random_file_number):
+                    if human_or_chatGPT != str(file_number):
                         if human_or_chatGPT == "1":
                             print("You are wrong. This poem was written by ChatGPT.")
                         if human_or_chatGPT == "2":
-                            print("You are wrong. This poem was written by " + str(author_name) + ".")
+                            print("You are wrong. This poem was written by ", author_name + ".")
 
                     times_looped += 1
                     accuracy = round(100 * (times_correct / times_looped))
@@ -113,5 +105,4 @@ while True:
                     print("Please write '1' for human or '2' for ChatGPT to proceed.\n")
                     continue
 
-# idea: kerää ohjelman vastaukset csv-tiedostoon
 
